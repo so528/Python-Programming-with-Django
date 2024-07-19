235dcbdc4ed82f025601deedb3ba52b88135909e
so528 <riseyoo5@naver.com>
2024-07-19 14:43:22 +0900

    Add files via upload

---
+++ 5단원/5-1.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
+    'books',               # 추가
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
